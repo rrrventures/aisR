86a47b59789ac609c72b222d4e509a629652067f
rrrventures <imauriacaf@gmail.com>
2018-12-07 16:36:36 -0300

    Added function to start project

--- R/funciones.R
+++ R/funciones.R
@@ -12,11 +12,11 @@ transform2Dummy <- function(variable, prefix = NULL) {
   # Didac, 20160909
   # variable: vector categorico a transformar a dummy
   # prefix: si queremos anyadir un prefijo a los colnames de la variable (formato character)
-  
+
   # Prevenim per quan hi ha NA's
   variable <- as.character(variable)
   variable[is.na(variable)] <- "NA"
-  
+
   result <- data.frame(sapply(unique(variable), function(x) {
     ifelse(variable == x, 1, 0)
   }))
@@ -101,11 +101,11 @@ guardar_variables_modelo <- function(base, variable_dependiente, directorio="", 
 train_test_split <- function(base, porc_val = 0.1, seed = 2017){
 	set.seed(seed)
 	ncons <- sample(1:nrow(base), nrow(base)*(1 - porc_val))
-	cons <- base[ncons, ]  
+	cons <- base[ncons, ]
 	test <- base[-ncons, ]
-	
+
 	lista <- list(cons = cons, test = test)
-	return(lista)	 
+	return(lista)
 }
 
 #' cargar_generico
@@ -139,4 +139,58 @@ completar_faltantes <- function(base_aevaluar, variables_onehot){
 	}
 
 	return(base_aevaluar)
+}
+
+
+#' iniciar_proyecto
+#'
+#' Crea las dummys que falten en la base
+#' @param PATH path al lugar donde se quiere iniciar un proyecto
+#' @param nombre nombre del proyecto, carpeta a crear
+#' @param autor nombre de quien trabajara sobre el proyecto
+#' @keywords inicio proyecto ais
+#' @export
+#' @examples
+iniciar_proyecto <- function(PATH = NULL, nombre = NULL, autor = NULL){
+
+  PATH <- paste0(PATH, '/', nombre, "/" )
+  DATADIR    <- paste(PATH, 'data',   sep = '')
+  INPUTDIR   <- paste(PATH, 'input',  sep = '')
+  OUTPUTDIR  <- paste(PATH, 'output', sep = '')
+  TEMPDIR    <- paste(PATH, 'temp',   sep = '')
+  SYNTAXDIR  <- paste(PATH, 'syntax', sep = '')
+  DOCDIR     <- paste(PATH, 'doc',    sep = '')
+  LOGDIR     <- paste(PATH, 'logs',    sep = '')
+  SCRIPTSDIR <- paste(PATH, 'scripts', sep = '')
+  FUNSDIR <- paste(PATH, 'funciones', sep = '')
+  #DEYDEDIR   <- paste(PREFIX, 'deyde/logs/', sep = '')
+  dir.create(PATH) ;    cat('Carpeta creada:', PATH,    '\n')
+  if (! file.exists(DATADIR))    { dir.create(DATADIR);    cat('Carpeta creada:', DATADIR,    '\n') }
+  if (! file.exists(INPUTDIR))   { dir.create(INPUTDIR);   cat('Carpeta creada:', INPUTDIR,   '\n') }
+  if (! file.exists(OUTPUTDIR))  { dir.create(OUTPUTDIR);  cat('Carpeta creada:', OUTPUTDIR,  '\n') }
+  if (! file.exists(TEMPDIR))    { dir.create(TEMPDIR);    cat('Carpeta creada:', TEMPDIR,    '\n') }
+  if (! file.exists(SYNTAXDIR))  { dir.create(SYNTAXDIR);  cat('Carpeta creada:', SYNTAXDIR,  '\n') }
+  if (! file.exists(DOCDIR))     { dir.create(DOCDIR);     cat('Carpeta creada:', DOCDIR,     '\n') }
+  if (! file.exists(LOGDIR))     { dir.create(LOGDIR);     cat('Carpeta creada:', LOGDIR,     '\n') }
+  if (! file.exists(SCRIPTSDIR)) { dir.create(SCRIPTSDIR); cat('Carpeta creada:', SCRIPTSDIR, '\n') }
+  if (! file.exists(FUNSDIR)) { dir.create(FUNSDIR); cat('Carpeta creada:', FUNSDIR, '\n') }
+
+  # Adaptamos rutas
+  DATADIR    <- paste(DATADIR,    '/', sep = '')
+  INPUTDIR   <- paste(INPUTDIR,   '/', sep = '')
+  OUTPUTDIR  <- paste(OUTPUTDIR,  '/', sep = '')
+  TEMPDIR    <- paste(TEMPDIR,    '/', sep = '')
+  SYNTAXDIR  <- paste(SYNTAXDIR,  '/', sep = '')
+  DOCDIR     <- paste(DOCDIR,     '/', sep = '')
+  LOGDIR     <- paste(LOGDIR,     '/', sep = '')
+  SCRIPTSDIR <- paste(SCRIPTSDIR, '/', sep = '')
+  FUNSDIR    <- paste(FUNSDIR,    '/', sep = '')
+
+  template <- template_inicio
+  template[3] <- paste0("# Autor(es)   : (c) ", autor, " ", Sys.time())
+  template[17] <- paste0("script <- paste('[", nombre, "] ", "01_Inicio Proyecto.R')")
+  template[25] <- paste0("PATH <- ", "'", PATH , "'")
+  writeLines(template, con = paste0(SYNTAXDIR, "000_inicioProyecto.R"))
+
+  cat("Proyecto ", nombre, "iniciado en ", PATH, "\n")
 }
